a3fa404b1845205229d5c3cdf96bb17868b73ec7
matthewsnell <matthewstuffacc@gmail.com>
2020-07-16 11:56:00 +0100

    add development branch and live environment

--- config.py
+++ config.py
@@ -6,6 +6,16 @@ if os.environ['FLASK_ENV'] == 'development':
     url = 'http://localhost:5000/'
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+elif os.environ['FLASK_ENV'] == 'development-live':
+    client_id = int(os.environ['STRAVA_CLIENT_ID'])
+    client_secret = os.environ['STRAVA_CLIENT_SECRET']
+    url = 'https://https://bulk-update-dev.herokuapp.com/'
+    # Celery:
+    CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+    CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
+    # Redis:
+    REDIS_URL = os.environ.get('REDIS_URL')
 else:
     client_id = int(os.environ['STRAVA_CLIENT_ID'])
     client_secret = os.environ['STRAVA_CLIENT_SECRET']
